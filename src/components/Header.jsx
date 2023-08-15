1456e2ded72404d91a12e3ce38580f464942a5df
Boluwatife <boluwatife1904@gmail.com>
2023-08-15 19:59:04 +0100

    adjustments

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,55 +18,57 @@ export default function Header() {
   }
 
   return (
-    <div className="shadow-md px-8 pt-8 flex items-center justify-between font-bold text-font1 w-[100%] -750:sticky -750:z-[25] -750:bg-white -750:py-6 -750:top-0">
-      <div>
-        <img className="hidden -750:block" src={logo} alt="" />
-      </div>
-
+    <>
       {/* overlay */}
       {isShown ? (
         <div
-          className="fixed bg-overlay top-0 left-0 h-[100%] w-[100%] z-[15]"
+          className="fixed bg-overlay top-0 left-0 h-[100%] w-[100%] z-[10]"
           onClick={handleMenu}
         ></div>
       ) : (
         ''
       )}
 
-      <div
-        ref={ham}
-        onClick={handleMenu}
-        className={`hamburger ${isShown ? 'active' : ''} ${not}`}
-      >
-        <div className="ham-bar ham-bar-1"></div>
-        <div className="ham-bar ham-bar-2"></div>
-        <div className="ham-bar ham-bar-3"></div>
-      </div>
+      <div className="relative shadow-md px-8 pt-8 flex items-center justify-between font-bold text-font1 w-[100%] -750:sticky -750:z-[25] -750:bg-white py-6 -750:top-0">
+        <div>
+          <img className="hidden -750:block" src={logo} alt="" />
+        </div>
 
-      <div
-        className={`navbar ${
-          isShown ? 'active' : ''
-        } px-8 flex items-center justify-between font-bold text-font1 w-[100%]`}
-      >
-        {/* left of header */}
+        <div
+          ref={ham}
+          onClick={handleMenu}
+          className={`hamburger ${isShown ? 'active' : ''} ${not}`}
+        >
+          <div className="ham-bar ham-bar-1"></div>
+          <div className="ham-bar ham-bar-2"></div>
+          <div className="ham-bar ham-bar-3"></div>
+        </div>
 
-        <nav>
-          <ul className="flex items-center gap-4">
-            <li className="mr-3">
-              <img className="-750:hidden" src={logo} alt="" />
-            </li>
-            <li>Features</li>
-            <li>Pricing</li>
-            <li>Resources</li>
-          </ul>
-        </nav>
+        <div
+          className={`navbar ${
+            isShown ? 'active' : ''
+          } px-8 flex items-center justify-between font-bold text-font1 w-[100%]`}
+        >
+          {/* left of header */}
 
-        {/* right of header */}
-        <div className="flex items-center gap-4 -750:w-[100%]">
-          <span className="cursor-pointer">Log in</span>
-          <Button style={'rounded-3xl -750:w-[80%]'} word={'Sign Up'} />
+          <nav>
+            <ul className="flex items-center gap-4">
+              <li className="mr-3">
+                <img className="-750:hidden" src={logo} alt="" />
+              </li>
+              <li>Features</li>
+              <li>Pricing</li>
+              <li>Resources</li>
+            </ul>
+          </nav>
+
+          {/* right of header */}
+          <div className="flex items-center gap-4 -750:w-[100%]">
+            <span className="cursor-pointer">Log in</span>
+            <Button style={'rounded-3xl -750:w-[80%]'} word={'Sign Up'} />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
